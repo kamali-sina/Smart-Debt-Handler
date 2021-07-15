7844a8e4a7c088f76022b807d9bddd003daf5d9d
sinku <sininoir@gmail.com>
2021-07-15 18:46:30 +0430

    Complete base description of the smar contract

--- BlockchainSplitwise.sol
+++ BlockchainSplitwise.sol
@@ -1,3 +1,30 @@
-// Please paste your contract's solidity code here
-// Note that writing a contract here WILL NOT deploy it and allow you to access it from your client
-// You should write and develop your contract in Remix and then, before submitting, copy and paste it here
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title BlockchainSplitwise
+ * @dev Store & retrieve value in a variable
+ */
+contract BlockchainSplitwise {
+
+    uint256 number;
+
+    /**
+     * @dev Add new IOU to database
+     * @param creditor address of user giving the IOU to
+     * @param amount value of the IOU being sent to creditor
+     */
+    function add_IOU(address creditor, uint32 amount) public {
+        
+    }
+    
+    /**
+     * @dev Return value of the debt of debtor to creditor
+     * @return ret debt value of debtor to creditor
+     */
+    function lookup(address debtor, address creditor) public view returns (uint32 ret) {
+        ret = 2; /* TODO: someshit */
+    }
+
+}
