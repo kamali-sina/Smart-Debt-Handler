7d4e88470a139fcd5cdcd08c4550db24c4cd506c
sinku <sininoir@gmail.com>
2021-07-16 16:22:39 +0430

    Completed base IOU logic

--- BlockchainSplitwise.sol
+++ BlockchainSplitwise.sol
@@ -8,7 +8,7 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 contract BlockchainSplitwise {
 
-    uint256 number;
+    mapping (address => mapping (address => uint32)) private _IOUs;
 
     /**
      * @dev Add new IOU to database
@@ -17,7 +17,7 @@ contract BlockchainSplitwise {
      */
     function add_IOU(address creditor, uint32 amount) public {
         require (amount >= 0, "Amount should be a positive value.");
-        
+        _IOUs[msg.sender][creditor] += amount;
     }
     
     /**
@@ -25,7 +25,13 @@ contract BlockchainSplitwise {
      * @return ret debt value of debtor to creditor
      */
     function lookup(address debtor, address creditor) public view returns (uint32 ret) {
-        ret = 2; /* TODO: someshit */
+        ret = _IOUs[debtor][creditor];
     }
+    
+    // function testt(address abs) public returns (uint32) {
+    //     add_IOU(abs, 22);
+    //     add_IOU(abs, 1);
+    //     return lookup(msg.sender, abs);
+    // }
 
 }
